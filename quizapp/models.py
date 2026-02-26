27a6a4c5512daf7049c4136e6dd018cb9ad4ba4b
Nitesh Meena <neeteshmaran0123@gmail.com>
2026-02-26 18:50:53 +0530

    questions model is added

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create your models 
+
+Difficulty_Chioces = [
+    ('easy', 'Easy'),
+    ('medium', 'Medium'),
+    ('hard', 'Hard'),
+]
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    difficulty = models.CharField(max_length=10, choices=Difficulty_Chioces,default='medium')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): #
+        return f"[{self.difficulty}] {self.text:[:50]}..."
